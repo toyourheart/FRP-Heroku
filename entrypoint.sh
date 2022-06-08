1625fa3b205c28fe7ce451f661c8a31c232e815d
toyourheart <wei40680@gmail.com>
2022-06-08 12:38:17 +0800

    wget frp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+# Setup FRP
+wget https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz \
+    && tar -xf frp_${FRP_VERSION}_linux_amd64.tar.gz \
+    && mkdir /frps \
+    && cp frp_${FRP_VERSION}_linux_amd64/frps* /frps/ \
+    && rm -rf frp_${FRP_VERSION}_linux_amd64*
+
+# Clean APK cache
+rm -rf /var/cache/apk/*
 
 # Dynamic set frps.ini based on PORT
 cd /frps
 cat <<-EOF > /frps/frps.ini
 [common]
 bind_port = $PORT
-
-dashboard_port = 7500
-# dashboard's username and password are both optional
-dashboard_user = admin
-dashboard_pwd = admin
 EOF
 
-echo "Running FRP Server :D"
-echo "Binding on the port $PORT"
 /frps/frps -c /frps/frps.ini

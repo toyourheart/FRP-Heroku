46fb6f13c7243ef87940875b8684739f300be2ad
toyourheart <wei40680@gmail.com>
2022-06-08 11:19:23 +0800

    add dashboard port

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ cd /frps
 cat <<-EOF > /frps/frps.ini
 [common]
 bind_port = $PORT
+
+dashboard_port = 7500
+# dashboard's username and password are both optional
+dashboard_user = admin
+dashboard_pwd = admin
 EOF
 
 echo "Running FRP Server :D"
